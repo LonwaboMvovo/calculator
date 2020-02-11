9cd451974d90e66d3f3ef764bdcf6305bc84310e
Lonwabo M <44nator@gmail.com>
2020-02-11 22:24:41 +0200

    made changes to all files

--- script.js
+++ script.js
@@ -1,32 +1,57 @@
-// Old Code:
-// var numbers = document.getElementsByClassName("numbers");
-// var functions = document.querySelectorAll(".function");
-// var equals = document.querySelector("#equals");
-// var answer = document.querySelector("#answer");
+let numbers = document.querySelectorAll('.numbers');
+let functions = document.querySelectorAll('.functions');
+let equal = document.querySelector('#equal');
+let clear = document.querySelector('#clear');
+let back = document.querySelector('#back');
+let divide = document.querySelector('#divide');
+let multiply = document.querySelector('#multiply');
+let answer = document.querySelector('#answer');
 
-// var num1 = [];
+let calculation = '';
+let userCalculation = '';
 
-// for (var i = 0; i < numbers.length; i++) {
-//     numbers[i].addEventListener("click", function() {
-//         num1.push(this.textContent);
-//         answer.textContent = num1;
-//     })
-// }
+for (var i = 0; i < numbers.length; i++) {
+    numbers[i].addEventListener('click', function() {
+        calculation += this.textContent;
+        userCalculation += this.textContent;
+        answer.textContent = userCalculation;
+    });
+}
 
-// for (var i = 0; i < numbers.length; i++) {
-//     numbers[i].addEventListener("click", function() {
-//         var answer1 = num1 + this.textContent;
-//         answer.textContent = answer1;
-//         done = false;
-//     })
-// }
+for (var i = 0; i < functions.length; i++) {
+    functions[i].addEventListener('click', function() {
+        calculation += this.textContent;
+        userCalculation += this.textContent;
+        answer.textContent = userCalculation;
+    })
+}
 
-// for (var i = 0; i < functions.length; i++) {
-//     functions[i].addEventListener("click", function() {
-//         num1.push(this.textContent);
-//         answer.textContent = num1;
-//     })
-// }
+equal.addEventListener('click', function() {
+    calculation = eval(calculation);
+    userCalculation = calculation;
+    answer.textContent = calculation;
+})
 
+clear.addEventListener('click', function() {
+    calculation = '';
+    userCalculation = '';
+    answer.textContent = calculation;
+})
 
-//New Code:
+back.addEventListener('click', function() {
+    calculation = calculation.slice(0, -1);
+    userCalculation = userCalculation.slice(0, -1);
+    answer.textContent = userCalculation;
+})
+
+divide.addEventListener('click', function() {
+    calculation += '/';
+    userCalculation += '÷';
+    answer.textContent = userCalculation;
+})
+
+multiply.addEventListener('click', function() {
+    calculation += '*';
+    userCalculation += '×'
+    answer.textContent = userCalculation;
+})
