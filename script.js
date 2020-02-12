8a212434fe33d686c64d61d72c3bb0f3a8d82e33
Lonwabo M <44nator@gmail.com>
2020-02-12 23:15:34 +0200

    fixed bugs

--- script.js
+++ script.js
@@ -11,47 +11,70 @@ let calculation = '';
 let userCalculation = '';
 
 for (var i = 0; i < numbers.length; i++) {
-    numbers[i].addEventListener('click', function() {
-        calculation += this.textContent;
-        userCalculation += this.textContent;
-        answer.textContent = userCalculation;
+    numbers[i].addEventListener('click', function() { 
+        if (userCalculation !== 'Ans' && calculation.length === 13) {
+            calculation += this.textContent;
+            userCalculation += this.textContent;
+            answer.value = userCalculation;
+        }
     });
 }
 
 for (var i = 0; i < functions.length; i++) {
     functions[i].addEventListener('click', function() {
-        calculation += this.textContent;
-        userCalculation += this.textContent;
-        answer.textContent = userCalculation;
+        calculation = calculation.toString();
+        if (calculation.charAt(calculation.length - 1) !== '+' && calculation.charAt(calculation.length - 1) !== '-' && calculation.charAt(calculation.length - 1) !== '/' && calculation.charAt(calculation.length - 1) !== '*' && calculation.length === 13) {
+            calculation += this.textContent;
+            userCalculation += this.textContent;
+            answer.value = userCalculation;
+    }
     })
 }
 
 equal.addEventListener('click', function() {
-    calculation = eval(calculation);
-    userCalculation = calculation;
-    answer.textContent = calculation;
+    if (calculation !== '' && calculation.charAt(calculation.length - 1) !== '+' && calculation.charAt(calculation.length - 1) !== '-' && calculation.charAt(calculation.length - 1) !== '*' && calculation.charAt(calculation.length - 1) !== '/') {
+        calculation = eval(calculation);
+        calculation = calculation.toString();
+        calculation = calculation.substr(0,13);
+        userCalculation = 'Ans';
+        answer.value = calculation;
+        calculation = parseFloat(calculation);
+    }
 })
 
 clear.addEventListener('click', function() {
     calculation = '';
     userCalculation = '';
-    answer.textContent = calculation;
+    answer.value = calculation;
 })
 
 back.addEventListener('click', function() {
-    calculation = calculation.slice(0, -1);
-    userCalculation = userCalculation.slice(0, -1);
-    answer.textContent = userCalculation;
+    if (typeof calculation === 'string') {
+        calculation = calculation.slice(0, -1);
+        userCalculation = userCalculation.slice(0, -1);
+        answer.value = userCalculation;
+    }
+    if (userCalculation.charAt(userCalculation.length - 1) === 'n') {
+        calculation = '';
+        userCalculation = '';
+        answer.value = calculation;
+    }
 })
 
 divide.addEventListener('click', function() {
-    calculation += '/';
-    userCalculation += '÷';
-    answer.textContent = userCalculation;
+    calculation = calculation.toString();
+    if (calculation.charAt(calculation.length - 1) !== '+' && calculation.charAt(calculation.length - 1) !== '-' && calculation.charAt(calculation.length - 1) !== '/' && calculation.charAt(calculation.length - 1) !== '*' && calculation.length < 13) {
+        calculation += '/';
+        userCalculation += '÷';
+        answer.value = userCalculation;   
+    }
 })
 
 multiply.addEventListener('click', function() {
-    calculation += '*';
-    userCalculation += '×'
-    answer.textContent = userCalculation;
+    calculation = calculation.toString();
+    if (calculation.charAt(calculation.length - 1) !== '+' && calculation.charAt(calculation.length - 1) !== '-' && calculation.charAt(calculation.length - 1) !== '/' && calculation.charAt(calculation.length - 1) !== '*' && calculation.length < 13) {
+        calculation += '*';
+        userCalculation += '×'
+        answer.value = userCalculation;
+    }
 })
